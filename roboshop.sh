dcc46225cfe6c57ce60cafe8e32c38f242639c63
sangala632 <sangala2003@gmail.com>
2025-11-27 07:33:59 +0530

    change the sg

--- roboshop.sh
+++ roboshop.sh
@@ -4,7 +4,7 @@ AMI_ID="ami-09c813fb71547fc4f"
 SG_ID="sg-0042dc4cebb7b918d" # replace with your SG ID
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
 ZONE_ID="Z100104611ELJF6AI8M2E" # replace with your ZONE ID
-DOMAIN_NAME="daws84s.site" # replace with your domain
+DOMAIN_NAME="hellodevsecops.space" # replace with your domain
 
 #for instance in ${INSTANCES[@]}
 for instance in $@
